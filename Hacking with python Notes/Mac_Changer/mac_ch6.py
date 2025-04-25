c3420d2fce852e17510cbc1dfaea5766d426e581
satanandmaurya2001 <satanand2001@gmail.com>
2025-04-25 10:40:06 -0400

    Lecture 2 update

--- Hacking with python Notes/Mac_Changer/mac_ch6.py
+++ Hacking with python Notes/Mac_Changer/mac_ch6.py
@@ -1,16 +1,33 @@
-#Handling User Input
+# Handling User Input
 
 #!/usr/bin/env python
+# This line tells the system to run the script with the default Python interpreter set in the environment.
+# Useful in Linux for executing the script directly from the terminal.
 
 import subprocess
+# Importing the subprocess module allows us to run system-level commands like 'ifconfig' from within Python.
 
-interface = input ("interface > ")
-new_mac = input (" new_mac >")
+interface = input("interface > ")
+# Taking input from the user for the name of the network interface (e.g., eth0, wlan0).
+# Example: eth0, wlan0 – which are typically used in networking.
+
+new_mac = input("new_mac > ")
+# Asking the user to input the new MAC address that they want to assign to the interface.
+# A MAC address is a unique identifier assigned to a network interface.
 
 print("[+] changing MAC address for " + interface + " to " + new_mac)
+# Displaying the action that is going to be taken – helpful for user confirmation.
 
+# The following commands change the MAC address using system tools via subprocess.
 
-# This command is more secure than previous 
 subprocess.call(["ifconfig", interface, "down"])
+# This disables the network interface before changing the MAC address.
+# Required because most systems don't allow MAC address changes while the interface is up.
+
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+# This changes the MAC address of the specified interface.
+# 'hw ether' is used to specify the new hardware (MAC) address.
+
 subprocess.call(["ifconfig", interface, "up"])
+# This re-enables the network interface after changing the MAC address.
+
--- Hacking with python Notes/Mac_Changer/mac_ch7.py
+++ Hacking with python Notes/Mac_Changer/mac_ch7.py
@@ -13,10 +13,11 @@ parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
 parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-parser.parse_args()
 
-interface = input ("interface > ")
-new_mac = input (" new_mac >")
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] changing MAC address for " + interface + " to " + new_mac)
 
@@ -25,3 +26,7 @@ print("[+] changing MAC address for " + interface + " to " + new_mac)
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
+
+
+
+
